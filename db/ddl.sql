989f54a444db3e7e5850ea7b71f42f36b2dd4874
Branwin DuBose <bdubose@uab.edu>
2018-10-05 12:00:16 -0500

    inital database model

---
+++ db/ddl.sql
@@ -0,0 +1,46 @@
+drop table if exists room;
+create table if not exists room
+( room_id   serial not null
+, room_name text   not null
+, constraint pk_room primary key (room_id) using index tablespace pg_default
+);
+
+drop table if exists obj;
+create table if not exists obj
+( obj_id     serial not null
+, room_id    int    not null
+, obj_name   text   not null
+, watts      int    not null -- maybe double later
+, is_on_open boolean -- is this thing on/off, open/closed? guess I'll leave this nullable for now
+, constraint pk_obj primary key (obj_id) using index tablespace pg_default
+, constraint fk_obj_1 foreign key (room_id) references room(room_id) on delete cascade   
+);
+create index ix_obj on obj using btree(room_id) tablespace pg_default;
+
+drop table if exists power_consumption;
+create table if not exists power_consumption
+( power_consumption_id serial
+, obj_id               int
+, duration             interval
+, watth                numeric  -- calculated and cached
+, constraint pk_power_consumption primary key (power_consumption_id) using index tablespace pg_default
+, constraint fk_power_consumption_1 foreign key (obj_id) references obj(obj_id) on delete cascade
+);
+
+drop table if exists power_bill;
+create table if not exists power_bill
+( power_bill_id serial
+, period        tstzrange
+, amount        numeric
+, kwh           numeric
+, constraint pk_power_bill primary key (power_bill_id) using index tablespace pg_default
+);
+
+drop table if exists temperature;
+create table if not exists temperature
+( temperature_id  serial
+, temp_time       timestamptz
+, is_outside_temp boolean
+, val             int -- might need to be a double?
+, constraint pk_temperature primary key (temperature_id) using index tablespace pg_default
+);
