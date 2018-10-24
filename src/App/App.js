c93c215c42ad0921dceefc3293a62efa744cad29
Matthew Jackson <mjacks36@outlook.com>
2018-10-24 15:47:10 -0500

    Manually merging Hayden's work with Master because of diffs

--- src/App/App.js
+++ src/App/App.js
@@ -9,23 +9,29 @@ import './App.css';
 import Hello from './pages/Hello';
 import List from './pages/List';
 import RoomList from './pages/RoomList';
+import WelcomePage from './pages/WelcomeTest';
+import AdminPage from './pages/AdminPage';
 
 class App extends Component {
   render() {
     return (
       <HashRouter>
             <div>
-                <h1>Simple SPA</h1>
+                <h1>Smart Home App</h1>
                 <ul className="header">
                     <li><NavLink exact to="/">Home</NavLink></li>
                     <li><NavLink to='/list'>List</NavLink></li>
                     <li><NavLink to='/roomList'>Room List</NavLink></li>
+                    <li><NavLink to='/test'>Welcome Test</NavLink></li>
+                    <li><NavLink to='/adminPage'>Admin Page</NavLink></li>
                 </ul>
                 <div className="content">
                     <Switch>
-                        <Route exact path="/" component={Hello}/>
-                        <Route path='/list' component={List}/>
-                        <Route path='/roomList' component={RoomList}/>
+                        <Route exact path="/" component={Hello} />
+                        <Route path='/list' component={List} />
+                        <Route path='/roomList' component={RoomList} />
+                        <Route path='/adminPage' component={AdminPage} />
+                        <Route path='/test' component={WelcomePage} />
                     </Switch>
                 </div>
             </div>
---
+++ src/App/pages/AdminPage.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import Accordion from "../components/Accordion/Accordion";
+
+class AdminPage extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default AdminPage;
