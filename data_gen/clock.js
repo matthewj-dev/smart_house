5a81ef3e7753c3c45a9087c61c6ff75c0712fe1c
Branwin DuBose <bdubose@uab.edu>
2018-11-04 22:15:17 -0600

    changed the name of the method to need less characters....

--- data_gen/clock.js
+++ data_gen/clock.js
@@ -2,7 +2,7 @@ realTimeCache = new Date();
 multiplier = 5.0;
 
 module.exports = {
-    getTime: () => {
+    now: () => {
         let now = new Date();
         let diff = now - realTimeCache;
         now.setTime(now.getTime() + (diff * multiplier));
